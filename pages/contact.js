957c165a8b87d00e632295faadd2de349d2bf6d6
Nicholas Gibb <nicholas.gibb@phac-aspc.gc.ca>
2021-01-08 16:24:26 -0500

    Add contact

--- pages/contact.js
+++ pages/contact.js
@@ -1,13 +1,31 @@
 import Head from "next/head";
 import Layout from "../components/layout/layout";
-import { Container } from "semantic-ui-react";
+import { Container, List } from "semantic-ui-react";
 
 export default function Home() {
   return (
     <Layout title={"Contact"}>
       <Container text>
         <h2>Contact</h2>
-        <p>Here goes some text!</p>
+        <List horizontal>
+          <List.Item>
+            <List.Content>
+              <List.Header>Kevin Brown</List.Header>
+              <a href="mailto:jack@semantic-ui.com">Kevin.Brown@example.ca</a>
+              <List.Description>University of Toronto</List.Description>
+            </List.Content>
+          </List.Item>
+
+          <List.Item>
+            <List.Content>
+              <List.Header>Nicholas Gibb</List.Header>
+              <a href="mailto:jack@semantic-ui.com">nicholas.gibb@example.ca</a>
+              <List.Description>
+                Public Health Agency of Canada
+              </List.Description>
+            </List.Content>
+          </List.Item>
+        </List>
       </Container>
     </Layout>
   );
