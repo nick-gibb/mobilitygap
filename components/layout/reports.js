ff25d5712730fb61cc0b4b74ad97a090b72754e3
Nicholas Gibb <nicholas.gibb@phac-aspc.gc.ca>
2021-01-09 16:26:51 -0500

    Update

--- components/layout/reports.js
+++ components/layout/reports.js
@@ -1,23 +1,5 @@
 import { Header, Placeholder, Statistic } from "semantic-ui-react";
 import Layout from "./layout";
-import styles from "./reports.module.css";
-import cn from "classnames";
-
-const mobility = Math.floor(Math.random() * 100) + 1;
-const mobility_threshold = Math.floor(Math.random() * 100) + 1;
-const mobility_gap = mobility - mobility_threshold;
-
-const mobility_gap_sign = mobility_gap < 0 ? "success" : "error";
-
-const items = [
-  { key: "mobility", label: "Mobility", value: mobility },
-  { key: "threshold", label: "Mobility Threshold", value: mobility_threshold },
-  { key: "gap", label: "Mobility Gap", value: mobility_gap },
-];
-
-const StatisticExampleGroupShorthand = () => (
-  <Statistic.Group widths="three" size="small" items={items} />
-);
 
 const PlaceholderExampleLine = () => (
   <Placeholder>
@@ -30,20 +12,34 @@ const PlaceholderExampleLine = () => (
 );
 
 export default function Report(props) {
-  const type = mobility_gap_sign;
+  const mobility = Math.floor(Math.random() * 100) + 1;
+  const mobility_threshold = Math.floor(Math.random() * 100) + 1;
+  const mobility_gap = mobility - mobility_threshold;
+  const mobility_gap_sign = mobility_gap < 0 ? "red" : "green";
 
+  const items = [
+    { key: "mobility", label: "Mobility", value: mobility },
+    {
+      key: "threshold",
+      label: "Mobility Threshold",
+      value: mobility_threshold,
+    },
+    {
+      key: "gap",
+      label: "Mobility Gap",
+      value: mobility_gap,
+      color: mobility_gap_sign,
+    },
+  ];
+
+  const StatisticExampleGroupShorthand = () => (
+    <Statistic.Group widths="three" size="small" items={items} />
+  );
   return (
     <Layout title={props.region}>
       <Header as="h2">Mobility Gap: {props.region}</Header>
       <StatisticExampleGroupShorthand />
-      <div
-        className={cn({
-          [styles.success]: type === "success",
-          [styles.error]: type === "error",
-        })}
-      >
-        This is a test
-      </div>
+
       <PlaceholderExampleLine />
     </Layout>
   );
