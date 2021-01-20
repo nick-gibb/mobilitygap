2ce0b5258c5eaa141ff9de8ad14de4f0f47fe59e
Nicholas Gibb <nicholas.gibb@phac-aspc.gc.ca>
2021-01-20 02:33:54 -0500

    Update

--- pages/trends.js
+++ pages/trends.js
@@ -2,6 +2,38 @@ import Layout from "../components/layout/layout";
 import React from "react";
 import { Container } from "semantic-ui-react";
 import { Header } from "semantic-ui-react";
+import { Line } from "react-chartjs-2";
+
+const data = {
+  datasets: [
+    {
+      data: [
+        {
+          x: 10,
+          y: 20,
+        },
+        {
+          x: 15,
+          y: 10,
+        },
+      ],
+      label: "Ontario",
+    },
+    {
+      data: [
+        {
+          x: 10,
+          y: 15,
+        },
+        {
+          x: 15,
+          y: 8,
+        },
+      ],
+      label: "Alberta",
+    },
+  ],
+};
 
 export default function Trends() {
   return (
@@ -12,6 +44,16 @@ export default function Trends() {
           content="Mobility Trends"
           dividing
           subheader="The latest mobility data from Google"
+        />
+
+        <Line
+          data={data}
+          options={{
+            title: {
+              display: true,
+              text: "Awesome Mobility Trends",
+            },
+          }}
         />
       </Container>
     </Layout>
