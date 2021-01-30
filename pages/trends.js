c1f79c8dd6ed30e02d8f358cfa8178371dd274a0
Nicholas Gibb <nicholas.gibb@phac-aspc.gc.ca>
2021-01-30 01:46:44 -0500

    Update

--- pages/trends.js
+++ pages/trends.js
@@ -3,7 +3,11 @@ import React from "react";
 import { Container } from "semantic-ui-react";
 import { Header } from "semantic-ui-react";
 import { Line } from "react-chartjs-2";
-import { Dropdown } from "semantic-ui-react";
+import { Dropdown, Segment } from "semantic-ui-react";
+
+const database = require("../lib/db");
+const pgp = database.pgp;
+const db = database.db;
 
 const data = {
   datasets: [
@@ -36,19 +40,34 @@ const data = {
   ],
 };
 
-export default function Trends() {
+export async function getStaticProps() {
+  const sql = pgp.as.format(
+    "SELECT distinct sub_region_2 FROM public.canada WHERE country_region=$1 AND sub_region_1 is not null AND sub_region_2 is not null",
+    "Canada"
+  );
+
+  const queryResult = await db.any("$1:raw", sql);
+
+  return {
+    props: { queryResult },
+  };
+}
+
+export default function Trends(props) {
+  console.log(props["queryResult"]);
+  const options = props["queryResult"].map((csd) => {
+    return {
+      key: csd["sub_region_2"],
+      value: csd["sub_region_2"],
+      text: csd["sub_region_2"],
+    };
+  });
+  console.log(options);
   return (
     <Layout title={"Trends"}>
       <Container text>
         <Header as="h1" content="Mobility Trends" />
-        <Dropdown placeholder="Province" fluid multiple search selection />
-        <Dropdown
-          placeholder="Census subdivision"
-          fluid
-          multiple
-          search
-          selection
-        />
+
         <Line
           data={data}
           options={{
@@ -57,6 +76,16 @@ export default function Trends() {
               text: "Awesome Mobility Trends",
             },
           }}
+        />
+        <Dropdown placeholder="Province" fluid multiple search selection />
+        <Dropdown
+          style={{ marginTop: 5 }}
+          placeholder="Census subdivision"
+          fluid
+          multiple
+          search
+          selection
+          options={options}
         />
       </Container>
     </Layout>
