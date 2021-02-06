bcd23cbda54150b111ef776db55dc815104137c9
Nicholas Gibb <nicholas.gibb@phac-aspc.gc.ca>
2021-02-06 13:54:47 -0500

    update

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Layout from "../components/layout/layout";
 import { Header, Icon, Container } from "semantic-ui-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -12,7 +13,19 @@ export default function Home() {
             We provide real-time information on cellular phone mobility measures
             and how they relate to COVID-19 transmission. On this site you can
             find regularly updated information on mobility across regions of
-            Canada.
+            Canada, including{" "}
+            <Link href="/trends">
+              <a>
+                <b>Mobility Trends</b>
+              </a>
+            </Link>{" "}
+            and{" "}
+            <Link href="/reports">
+              <a>
+                <b>Mobility Gap Regional Reports</b>
+              </a>
+            </Link>
+            .
           </Header.Subheader>
         </Header>
       </Container>
