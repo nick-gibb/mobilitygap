8993f8d587ae2883d2d1fe63d7538ce9fdd86385
Nicholas Gibb <nicholas.gibb@phac-aspc.gc.ca>
2021-02-07 14:10:09 -0500

    Update

--- pages/research.js
+++ pages/research.js
@@ -30,14 +30,18 @@ export default function Research() {
           subheader="Pre-prints and published peer-reviewed articles on mobility patterns
   and COVID-19"
         />
-        <Header
-          as="h4"
+        <Link
           href="https://www.medrxiv.org/content/10.1101/2021.01.28.21250622v1"
           target="_blank"
+          passHref
         >
-          The Mobility Gap: Mobility Reductions Needed to Suppress COVID-19
-          through the Winter Surge in Canada
-        </Header>
+          <a>
+            <Header as="h4">
+              The Mobility Gap: Mobility Reductions Needed to Suppress COVID-19
+              through the Winter Surge in Canada
+            </Header>
+          </a>
+        </Link>
         <p>
           Kevin A. Brown, Jean-Paul R. Soucy, Sarah A. Buchan, Shelby L.
           Sturrock, Isha Berry, Nathan M. Stall, Peter Juni, Amir Ghasemi,
@@ -45,44 +49,24 @@ export default function Research() {
         </p>
         <p>Manuscript under review.</p>
         <Divider />
-        <Item.Group link divided relaxed>
-          <Item
-            href="https://www.medrxiv.org/content/10.1101/2021.01.28.21250622v1"
-            target="_blank"
-          >
-            <Item.Content>
-              <Item.Header>
-                The Mobility Gap: Mobility Reductions Needed to Suppress
-                COVID-19 through the Winter Surge in Canada
-              </Item.Header>
-              <Item.Meta>
-                Kevin A. Brown, Jean-Paul R. Soucy, Sarah A. Buchan, Shelby L.
-                Sturrock, Isha Berry, Nathan M. Stall, Peter Juni, Amir Ghasemi,
-                Nicholas Gibb, Derek R. MacFadden, Nick Daneman
-              </Item.Meta>
-              <Item.Description>Manuscript under review</Item.Description>
-            </Item.Content>
-          </Item>
-          <Item
-            href="https://www.medrxiv.org/content/10.1101/2020.04.05.20054288v3"
-            target="_blank"
-          >
-            <Item.Content>
-              <Item.Header>
-                Estimating effects of physical distancing on the COVID-19
-                pandemic using an urban mobility index
-              </Item.Header>
-              <Item.Meta>
-                Jean-Paul R. Soucy, Shelby L. Sturrock, Isha Berry, Duncan J.
-                Westwood, Nick Daneman, Derek R. MacFadden, Kevin A. Brown
-              </Item.Meta>
-              <Item.Description>
-                <i>MedRxiv</i>
-              </Item.Description>
-              <Item.Description>May 24, 2020</Item.Description>
-            </Item.Content>
-          </Item>
-        </Item.Group>
+        <Link
+          href="https://www.medrxiv.org/content/10.1101/2020.04.05.20054288v3"
+          target="_blank"
+          passHref
+        >
+          <a>
+            <Header as="h4">
+              Estimating effects of physical distancing on the COVID-19 pandemic
+              using an urban mobility index
+            </Header>
+          </a>
+        </Link>
+        <p>
+          Jean-Paul R. Soucy, Shelby L. Sturrock, Isha Berry, Duncan J.
+          Westwood, Nick Daneman, Derek R. MacFadden, Kevin A. Brown
+        </p>
+        <p>MedRxiv</p>
+        <p>May 24, 2020</p>
       </Container>
     </Layout>
   );
