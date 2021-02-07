9e4aac62e30ea76fa93197ab3cb58f81fc8398c8
Nicholas Gibb <nicholas.gibb@phac-aspc.gc.ca>
2021-02-06 21:00:24 -0500

    Update

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -7,11 +7,41 @@ export default function Layout({ children, title }) {
   return (
     <div>
       <Head>
-        <title>Mobility Gap - {title} </title>
+        <title>Mobility Gap </title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta
           key="og:image"
           name="og:image"
+          content="https://mobilitygap.ca/figures/ca/2021-02-06/mobility_byMonth.png"
+        />
+
+        <meta
+          name="description"
+          content="Real-time information on cellular phone mobility measures and how they relate to COVID-19 transmission."
+        />
+
+        <meta property="og:url" content="https://mobilitygap.ca/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Mobility Gap" />
+        <meta
+          property="og:description"
+          content="Real-time information on cellular phone mobility measures and how they relate to COVID-19 transmission."
+        />
+        <meta
+          property="og:image"
+          content="https://mobilitygap.ca/figures/ca/2021-02-06/mobility_byMonth.png"
+        />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="mobilitygap.ca" />
+        <meta property="twitter:url" content="https://mobilitygap.ca/" />
+        <meta name="twitter:title" content="Mobility Gap " />
+        <meta
+          name="twitter:description"
+          content="Real-time information on cellular phone mobility measures and how they relate to COVID-19 transmission."
+        />
+        <meta
+          name="twitter:image"
           content="https://mobilitygap.ca/figures/ca/2021-02-06/mobility_byMonth.png"
         />
       </Head>
