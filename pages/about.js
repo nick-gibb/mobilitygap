9e4c5a8a8c269a92280cecacc52aa2ea01e76a4e
Nicholas Gibb <nicholas.gibb@phac-aspc.gc.ca>
2021-02-07 12:55:47 -0500

    update

--- pages/about.js
+++ pages/about.js
@@ -26,21 +26,21 @@ const BreadcrumbExample = () => (
 export default function About() {
   return (
     <Layout title={"About"}>
-      <Container text>
+      <Container>
         <BreadcrumbExample />
-        <PageTitle titleText="About" subheader="" />
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        <PageTitle titleText="Mobility Gap Team" subheader="" />
+        {/* Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
         commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
         velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
         occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </Container>
-      <Container>
-        <Divider horizontal>
+        mollit anim id est laborum. */}
+        {/* </Container>
+      <Container> */}
+        {/* <Divider horizontal>
           <Header as="h4">Mobility Gap Team</Header>
-        </Divider>
+        </Divider> */}
         <Grid stackable doubling columns={3}>
           <Grid.Row>
             <Grid.Column>
