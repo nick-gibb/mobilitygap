94ca29fa7fb68db53c67f7dd71bfb8dbce889d00
Nicholas Gibb <nicholas.gibb@phac-aspc.gc.ca>
2021-02-09 09:58:42 -0500

    Update

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,38 @@
 import Layout from "../components/layout/layout";
-import { Header, Icon, Container } from "semantic-ui-react";
+import { Header, Container } from "semantic-ui-react";
 import Link from "next/link";
+import { Image } from "semantic-ui-react";
 
 export default function Home() {
   return (
     <Layout title="Mobility Gap">
       <Container text>
-        <Header as="h2" icon textAlign="center">
-          <Icon name="street view" circular />
-          <Header.Content>Mobility Gap</Header.Content>
-          <Header.Subheader>
-            We provide real-time information on cellular phone mobility measures
-            and how they relate to COVID-19 transmission. On this site you can
-            find regularly updated information on mobility across regions of
-            Canada, including{" "}
-            <Link href="/trends">
-              <a>
-                <b>Mobility Trends</b>
-              </a>
-            </Link>{" "}
-            and{" "}
-            <Link href="/reports">
-              <a>
-                <b>Mobility Gap Regional Reports</b>
-              </a>
-            </Link>
-            .
-          </Header.Subheader>
+        <Image
+          src="/mglogo.png"
+          alt="Mobility Gap Logo"
+          width={263}
+          height={298}
+          responsive
+          style={{
+            margin: "0 auto",
+          }}
+        />
+        <Header as="h4" icon textAlign="center">
+          We provide real-time information on cellular phone mobility measures
+          and how they relate to COVID-19 transmission. On this site you can
+          find regularly updated information on mobility across regions of
+          Canada, including{" "}
+          <Link href="/trends">
+            <a>
+              <b>Mobility Trends</b>
+            </a>
+          </Link>{" "}
+          and{" "}
+          <Link href="/reports">
+            <a>
+              <b>Mobility Gap Regional Reports</b>
+            </a>
+          </Link>
         </Header>
       </Container>
     </Layout>
